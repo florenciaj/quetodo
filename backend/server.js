17de480803b3171ed6add9522cfbc3e3580c8a3e
florenciaj <f.lopez.pacini@gmail.com>
2023-05-14 20:03:01 -0300

    display taks on page

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-unused-vars
+const cors = require("cors");
 const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
@@ -9,7 +10,11 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+	origin: ["http://localhost:3000"]
+}));
 app.use('/api', taskRoute);
+
 
 // routes
 app.get('/', (req, res) => {
@@ -19,7 +24,7 @@ app.get('/', (req, res) => {
 const { PORT } = process.env;
 
 mongoose
-	.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true })
+	.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
 	.then(() => {
 		app.listen(PORT, () => {
 			console.log(`server running on port ${PORT}`);
